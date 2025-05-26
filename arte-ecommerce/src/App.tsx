3f4f6a3f1e7d76654e7835443a4b624c52173163
Gustavo Laporte <gustavo.laporte@finacap.com.br>
2025-05-26 17:14:19 -0300

    faixa tudo para criar

--- arte-ecommerce/src/App.tsx
+++ arte-ecommerce/src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Carrossel from "./components/Carrossel";
 import Banner from "./components/Banner";
 import Comunidade from "./components/Comunidade";
+import FaixaFundoBranco from "./components/FaixaFundoBranco";
+import FaixaTudoParaCriar from "./components/FaixaTudoParaCriar";
 import FaqSection from "./components/FaqSection";
 import Footer from "./components/Footer";
 
@@ -13,6 +15,12 @@ function App() {
         <Carrossel />
         <Banner />
         <Comunidade />
+        <div style={{ position: 'relative', width: '100vw' }}>
+          <FaixaFundoBranco />
+          <div style={{ position: 'absolute', top: 0, left: 0, width: '100vw', zIndex: 2 }}>
+            <FaixaTudoParaCriar />
+          </div>
+        </div>
         <FaqSection />
       </main>
       <Footer />
---
+++ arte-ecommerce/src/components/FaixaFundoBranco.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FaixaFundoBranco.css";
+
+const FaixaFundoBranco: React.FC = () => (
+  <div className="faixa-fundo-branco"></div>
+);
+
+export default FaixaFundoBranco; 
